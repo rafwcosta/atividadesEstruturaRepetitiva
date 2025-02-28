49eee3e049d1c4d7e725f29d5ae377344388cab6
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-02-28 16:03:39 -0300

    first commit

---
+++ src/Question01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Question01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        
+
+        sc.close();
+    }
+}
