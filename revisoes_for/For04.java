f42f1b50c46aab5a9ee511164ccaec7b767c6afb
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-10 15:52:28 -0300

    Repetitive structure review - For 04

---
+++ revisoes_for/For04.java
@@ -0,0 +1,31 @@
+package revisoes_for;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class For04 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor de N: ");
+        int n = sc.nextInt();
+
+        for (int i=0; i<n; i++) {
+            System.out.print("Digite o valor de X: ");
+            int x = sc.nextInt();
+            System.out.print("Digite o valor de Y: ");
+            int y = sc.nextInt();
+
+            if (y == 0) {
+                System.out.println("Divisão impossível.");
+            }
+            else {
+                double div = (double) x / y;
+                System.out.printf("%.1f%n", div);
+            }
+        }
+
+        sc.close();
+    }
+}
