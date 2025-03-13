f2deceee8891175ab73e63b11ddb9bf76598f37f
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-13 09:29:28 -0300

    Class Rename

--- funcoes_string/Split01.java
+++ funcoes_string/Split01.java
@@ -3,7 +3,7 @@ package funcoes_string;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Slipt01 {
+public class Split01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
