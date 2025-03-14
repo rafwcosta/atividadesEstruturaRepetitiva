60940b66980aa091ffcc5d92497ac6d48aa74249
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-03-14 16:36:14 -0300

    Atividade 07 de estrutura repetitiva com "for"

---
+++ revisoes_for/For07.java
@@ -0,0 +1,23 @@
+package revisoes_for;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class For07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Digite o valor de N: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            int primeiro = i;
+            int segundo = i * i;
+            int terceiro = i * i * i;
+            System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+        }
+
+        sc.close();
+    }
+}
